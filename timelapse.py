9505434a5814fcd7a851b68ad473596e61ebe267
Ian Smith <imsmith@uw.edu>
2012-10-26 16:03:27 -0700

    switching over to use numpy arrays rather an imgs

--- timelapse.py
+++ timelapse.py
@@ -23,21 +23,21 @@ import numpy as np
 import scipy.signal
 import scipy.ndimage
 
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     import pdb
     import Image as Img
 
-def get_offset(im1, im2, im1_rect=None, im2_rect=None, hamming=False):
-    """Get the translation offset between two similar images that are 
+def get_offset(arr1, arr2, arr1_rect=None, arr2_rect=None, hamming=False):
+    """Get the translation offset between two similar image matrices that are 
     displaced by some (x, y).  
 
-    A correlation calculated between im1 and im2 is used to determine 
-    the amount that im2 should be translated to align with im1.
+    A correlation calculated between arr1 and arr2 is used to determine 
+    the amount that arr2 should be translated to align with arr1.
 
-    If im1_rect or im2_rect are given, only the regions defined by those 
-    rectangles are considered in the correlation. This has the added benefit 
+    If arr1_rect or arr2_rect are given, only the regions defined by those 
+    rectangles are considered in the correlation. This has the benefit 
     of possibly speeding up the calculation.
 
     If hamming is True then a hamming window is applied to both image matrices
@@ -45,52 +45,71 @@ def get_offset(im1, im2, im1_rect=None, im2_rect=None, hamming=False):
     better.
 
     Args:
-        im1: A 2D numpy array representing an image.
-        im2: A 2D numpy array representing an image that will be attempted to
-            align with im1.
-        im1_rect: A tuple of coordinate pairs ((x1, y1), (x2, y2)) defining 
-            a rectangle in im1 to use for alignment.
-        im2_rect: A tuple of coordinate pairs ((x1, y1), (x2, y2)) defining 
-            a rectangle in im2 to use for alignment.
+        arr1: A 2D numpy array representing an image.
+        arr2: A 2D numpy array representing an image that will be attempted to
+            align with arr1.
+        arr1_rect: A tuple of coordinate pairs ((x1, y1), (x2, y2)) defining 
+            a rectangle in arr1 to use for alignment.
+        arr2_rect: A tuple of coordinate pairs ((x1, y1), (x2, y2)) defining 
+            a rectangle in arr2 to use for alignment.
         hamming: If True, a hamming window is applied to both arrays before
             correlation.
     
     Returns:
-        A tuple (x, y) representing the offset of the top left corner of im2
-        when aligned with the top left corner of im1.
+        A tuple (x, y) representing the offset of the top left corner of arr2
+        when aligned with the top left corner of arr1.
 
     Requires:
-        If im1_rect or im2_rect are given, they must define rectangles within
-        the bounds of im1 and im2, respectively.
+        If arr1_rect or arr2_rect are given, they must define rectangles within
+        the bounds of arr1 and arr2, respectively.
     """
     
-    if im1_rect is None:
-        (h, w) = im1.shape
-        im1_rect = ((0, 0), np.unravel_index(im1.size-1, (w, h)))
-    if im2_rect is None:
-        (h, w) = im2.shape
-        im2_rect = ((0, 0), np.unravel_index(im2.size-1, (w, h)))
-    # TODO: assert im1_rect and im2_rect are within im1, im2 bounds
+    if arr1_rect is None:
+        (h, w) = arr1.shape
+        arr1_rect = ((0, 0), np.unravel_index(arr1.size-1, (w, h)))
+    if arr2_rect is None:
+        (h, w) = arr2.shape
+        arr2_rect = ((0, 0), np.unravel_index(arr2.size-1, (w, h)))
+    # TODO: assert arr1_rect and arr2_rect are within arr1, arr2 bounds
     
-    # TODO: if hamming, then hamm up im1 and im2
-    
-    xcorr = scipy.signal.fftconvolve(im1, im2[::-1, ::-1])
-    """Do the cross-correlation, which requires flipping im2"""
+    """Slice out the sub rectangles"""
+    subrect1 = arr1[arr1_rect[0][1]:arr1_rect[1][1], arr1_rect[0][0]:arr1_rect[1][0]]
+    subrect2 = arr2[arr2_rect[0][1]:arr2_rect[1][1], arr2_rect[0][0]:arr2_rect[1][0]]
+
+    if hamming:
+      """Create the hamming template for subrect1 and 2 """
+      hamm255_1 = np.empty(subrect1.shape)
+      hamm255_1.fill(255)
+      hamm255_2 = np.empty(subrect2.shape)
+      hamm255_2.fill(255)
+
+      hamm255_1 = np.outer(np.hamming(hamm255_1.shape[0]),
+          np.hamming(hamm255_1.shape[1]))
+      hamm255_2 = np.outer(np.hamming(hamm255_2.shape[0]),
+          np.hamming(hamm255_2.shape[1]))
+      subrect1 = subrect1 * hamm255_1
+      subrect2 = subrect2 * hamm255_2
+
+    """Do the cross-correlation, which requires flipping arr2"""
+    xcorr = scipy.signal.fftconvolve(subrect1, subrect2[::-1, ::-1])
     
     if DEBUG:
         _show_array(xcorr)
         _save_array(xcorr, "images/xcorr_test.png")
 
     xcorr_max = np.unravel_index(xcorr.argmax(), xcorr.shape)
-    offset = (xcorr_max[1] - im2_rect[1][0], xcorr_max[0] - im2_rect[1][1])
+    offset = (xcorr_max[1] - arr2_rect[1][0], xcorr_max[0] - arr2_rect[1][1])
     
     return offset
     
+# TODO:  Needs a flag for expanding image to avoid crop
+# TODO:  Why flip the shifting???
 def shift(arr, shift):
-    return scipy.ndimage.shift(arr, shift)
+    sflip = (shift[1], shift[0]) 
+    return scipy.ndimage.shift(arr, sflip)
     
-def align(im1, im2):
-    return shift(im2, get_offset(im1, im2))
+def align(im1, im2, arr1_rect=None):
+    return shift(im2, get_offset(im1, im2, arr1_rect=arr1_rect, hamming=True))
 
 def _show_array(arr):
     _array_to_image(arr).show()
@@ -102,3 +121,9 @@ def _array_to_image(arr):
     img = arr * 255 / arr.max()
     img = Img.fromarray(np.uint8(img))
     return img
+
+def convertTo():
+    raise NotImplementedError
+
+def convertFrom():
+    raise NotImplementedError
